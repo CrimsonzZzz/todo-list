e734a974170c0fd30737de59e05697fb58c24660
CrimsonzZzz <matiasfndz27@gmail.com>
2026-05-24 13:30:20 -0500

    Agregado

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import "./style.css"
+
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const list = document.getElementById("taskList");
+
+button.addEventListener("click", () => {
+  const taskText = input.value;
+
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+  li.classList.add("bg-gray-200", "p-3", "rounded-lg", "mb-2", "shadow");
+
+  list.appendChild(li);
+
+  input.value = "";
+});
